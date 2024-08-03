c8c9fb503300af6a783797414d3b99df1f037527
Farrux <23@gmail.com>
2024-08-03 14:17:46 +0500

    fara

---
+++ main.js
@@ -0,0 +1,39 @@
+
+
+
+var myword = prompt ( 'Введите своё имя')
+
+console.log('Меня зовут '+ myword);
+
+var myage = prompt ( 'Введите свой возрост')
+
+console.log('Мне ' + myage);
+
+var task1 = prompt ( 'Решите Пример 5 + 5 = ')
+
+console.log('Пример 1: 5 + 5 = 10 Ваш ответ ' + task1 );
+
+var task2 = prompt ( 'Решите Пример 15 - 5 = ')
+
+console.log('Пример 2: 15 - 5 = 10 Ваш ответ ' + task2);
+
+var task3 = prompt ( 'Решите Пример 35 * 5 = ')
+
+console.log('Пример 3: 35 * 5 = 175 Ваш ответ ' + task3);
+
+var task4 = prompt ( 'Решите Пример 25 / 5 = ')
+
+console.log('Пример 4: 25 / 5 = 5 Ваш ответ ' + task4);
+
+var task5 = prompt ( 'Решите Пример 95 % 5 = ')
+
+console.log('Пример 5: 95 % 5 = 19 Ваш ответ ' + task5);
+
+
+alert  ( 'Откройте консоль для просмотра' )
+
+
+
+
+
+
